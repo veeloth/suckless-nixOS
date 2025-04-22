80f89e49e616cf1ab8c3d83f2f6d30996f53052b
veeloth <qweeloth@gmail.com>
2025-04-22 16:24:18 -0300

    added and fixed pollymc url

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     nixvim.url = "github:nix-community/nixvim/nixos-24.11";
     vebar.url = "path:pkgs/vebar";
     nhkd.url = "path:pkgs/nhkd";
-    pollymc.url = "github:2006/pollymc/develop";
+    pollymc.url = "github:fn2006/pollymc/develop";
   };
   outputs = { self, nixpkgs, nixvim, vebar, nhkd, pollymc, ... }@attrs: {
     nixosConfigurations.vlth = nixpkgs.lib.nixosSystem {
