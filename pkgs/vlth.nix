4c990c03422a661d146ee5ed29a7d16d73a4c839
veeloth <qweeloth@gmail.com>
2025-05-17 21:26:04 -0300

    readded osu!

--- pkgs/vlth.nix
+++ pkgs/vlth.nix
@@ -13,7 +13,7 @@ environment.systemPackages = with pkgs;
     btop binutils_nogold clang cordless dmenu dissent discordo
     fastfetch feh ffmpeg gh git gpick gruvbox-dark-gtk hmcl kitty lf
     librewolf mtpaint mpv nasm neovim vlc obs-studio
-    obsidian /*osu-lazer-bin*/ pamixer qemu scrot
+    obsidian osu-lazer-bin pamixer qemu scrot
     (st.overrideDerivation
       (old: {
       src = ./st;
