fb1059671c2765d3a537c3149868e0bf8ac1f94f
veeloth <qweeloth@gmail.com>
2025-06-05 15:54:28 -0300

    adding latest osu version manually attempt 2

--- pkgs/vlth.nix
+++ pkgs/vlth.nix
@@ -13,7 +13,13 @@ environment.systemPackages = with pkgs;
     btop binutils_nogold clang dmenu discordo
     fastfetch feh ffmpeg gh git gpick hmcl kitty lf
     librewolf mpv nasm neovim vlc obs-studio
-    obsidian osu-lazer-bin pamixer qemu scrot
+    obsidian
+    (osu-lazer-bin.overrideDerivation
+      (old: {
+      version = "2025.605.3";
+      })
+    )
+    pamixer qemu scrot
     (st.overrideDerivation
       (old: {
       src = ./st;
