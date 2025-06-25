90899186153033870353c004a7874403e130b968
veeloth <qweeloth@gmail.com>
2025-06-25 00:31:19 -0300

    suck dick

--- pkgs/vlth.nix
+++ pkgs/vlth.nix
@@ -13,7 +13,7 @@ environment.systemPackages = with pkgs;
   [
     appimage-run btop binutils_nogold clang ctags dmenu discordo
     fastfetch feh ffmpeg gh git gpick hmcl kitty lf
-    librewolf mpv nasm neovim quaternion vlc obs-studio
+    librewolf mpv nasm neovim fluffychat vlc obs-studio
     obsidian p7zip pamixer qemu scrot sdl3
     (st.overrideDerivation
       (old: {
