37b2263591a22e5235f53e9fd35d8eee155288a7
System administrator <qweeloth@gmail.com>
2025-07-07 19:03:31 -0300

    added dwm bg

--- pkgs/dwm/config.h
+++ pkgs/dwm/config.h
@@ -7,7 +7,7 @@ static const void startup()
   system("vebar &disown");
   //system("vesktop &disown");
   system("librewolf web.whatsapp.com &disown");
-  system("feh --bg-fill \"/home/veeloth/main/wallpapers/1e1d1c.png\"");
+  system("feh --bg-fill \"/home/veeloth/main/wallpapers/bg.png\"");
   system("nhkd &disown");
   system("~/dcstart &disown");//we use a bashscript to start discord so it we don't expose the token on github when uploading the repo
   };
